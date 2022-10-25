1494020fe48d47e732ecc9db973fadc083281982
Jorge <ferco_clpm@hotmail.com>
2022-10-25 07:56:50 -0400

    CV - version 2

---
+++ .eleventy.js
@@ -0,0 +1,21 @@
+const pageHeading = require("./src/_includes/shortcodes/pageHeading");
+
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("src/assets/css/style.css");
+  eleventyConfig.addPassthroughCopy("src/assets/images");
+
+  eleventyConfig.addShortcode("pageHeading", pageHeading);
+  eleventyConfig.addShortcode("currentYear", function() {
+    const today = new Date();
+    return today.getFullYear().toString();
+  });
+
+  return {
+    dir: {
+      input: "src",
+      data: "_data",
+      includes: "_includes",
+      layouts: "_layouts"
+    }
+  };
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,24 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-  darkMode: false, // or 'media' or 'class'
+  content: ["./src/**/*.{njk,md,js}", "./src/**/*.svg",],
   theme: {
     extend: {
-      colors: {
-        primary: '#6AAF92',
-        secondary: '#474747'
+      keyframes: {
+        shake: {
+          '10%': { transform: 'translate3d(-1px, 0, 0)' },
+          '90%': { transform: 'translate3d(-1px, 0, 0)' },
+          '20%': { transform: 'translate3d(1px, 0, 0)' },
+          '80%': { transform: 'translate3d(1px, 0, 0)' },
+          '30%': { transform: 'translate3d(-2px, 0, 0)' },
+          '50%': { transform: 'translate3d(-2px, 0, 0)' },
+          '70%': { transform: 'translate3d(-2px, 0, 0)' },
+          '40%': { transform: 'translate3d(2px, 0, 0)' },
+          '60%': { transform: 'translate3d(2px, 0, 0)' },
+        },
       },
-      borderWidth: {
-        '16': '16px',
+      animation: {
+        'shake-icon': 'shake 0.82s cubic-bezier(.36,.07,.19,.97) both',
       },
-      dropShadow: {
-        'text': '0px 4px 4px rgba(42, 42, 42, 0.25)',
-        'text-primary': '0px 4px 4px rgba(85, 85, 85, 0.25)',
-      }
-    }
-  },
-  variants: {
-    extend: {
-      /*transitionProperty: ['hover'],*/
     },
   },
   plugins: [],
