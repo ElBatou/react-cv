5fcb656a1914ff7119d3ce373e965558c09b1c32
Jorge Cortez <ferco_clpm@hotmail.com>
2025-09-29 09:16:54 -0400

    feat: update 11ty to v2, add new project and skills, fix hot reloading css issue

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,11 +1,11 @@
 const pageHeading = require("./src/_includes/shortcodes/pageHeading");
 
 module.exports = function (eleventyConfig) {
-  eleventyConfig.addPassthroughCopy("src/assets/css/tailwind.css");
+  //eleventyConfig.addPassthroughCopy("src/assets/css/tailwind.css");
   eleventyConfig.addPassthroughCopy("src/assets/images");
 
   eleventyConfig.addShortcode("pageHeading", pageHeading);
-  eleventyConfig.addShortcode("currentYear", function() {
+  eleventyConfig.addShortcode("currentYear", function () {
     const today = new Date();
     return today.getFullYear().toString();
   });
@@ -16,7 +16,7 @@ module.exports = function (eleventyConfig) {
       input: "src",
       data: "_data",
       includes: "_includes",
-      layouts: "_layouts"
-    }
+      layouts: "_layouts",
+    },
   };
-}
+};
